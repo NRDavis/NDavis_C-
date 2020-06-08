a8b694e0dcf709d0f66e880438bdac003c0348f5
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-08 03:49:01 -0400

    working video stream

---
+++ OpenCV/cvTest/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+/*
+Following this tutorial allowed us to use opencv with C++:
+https://medium.com/@sourabhjigjinni/install-opencv-4-0-0-for-c-windows-7-10-code-blocks-tdm-gcc-64-dff65addf162
+
+
+*/
+
+int main() {
+
+	VideoCapture cap(0);
+	if (!cap.isOpened()) {
+		cout << "Error initializing video camera!" << endl;
+		return -1;
+	}
+
+	char* windowName = "Webcam Feed";
+	namedWindow(windowName, WINDOW_AUTOSIZE);
+
+	while (1) {
+
+		Mat frame;
+		bool bSuccess = cap.read(frame);
+
+		if (!bSuccess) {
+			cout << "Error reading frame from camera feed" << endl;
+			break;
+		}
+
+		imshow(windowName, frame);
+		switch (waitKey(10)) {
+		case 27:
+			return 0;
+		}
+	}
+	return 0;
+}
