782ae6798f49e8c20d2ca6443a08bd02db5fa10b
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-14 03:01:20 -0400

    adding pseudo code to insertionsort basic

--- sort/sorting/insertionsort.cpp
+++ sort/sorting/insertionsort.cpp
@@ -5,9 +5,14 @@ using namespace std;
 template <typename T>
 void insertionBasic(T a[], int mag)
 {
-    int i;
-    int low = 0;
-    int high = mag - 1;
+    int i = 1;
+    int j = 1;
+    while(i < mag){
+        while(j > 0 && A[j-1] > A[j]){
+
+        }
+        i++;
+    }
 
 
     return;
--- sort/sorting/main.cpp
+++ sort/sorting/main.cpp
@@ -8,6 +8,11 @@ void printArray(T A[], int mag);
 template <typename T>
 void dumbSort(T A[], int mag);
 
+
+
+
+
+
 int main()
 {
     int a[10] = {0,1,2,3,4,5,6,7,8,9};
@@ -25,6 +30,13 @@ int main()
     //insertionBasic(a, 2);
     return 0;
 }
+
+
+
+
+
+
+
 
 template <typename T>
 void printArray(T A[], int mag)
